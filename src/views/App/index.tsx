5abca278280a3faa99be3d8de3eb7914697a50cb
Carlos Ceniceros <juank_kenny@hotmail.com>
2022-12-05 23:13:48 -0600

    Initial code for project

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,8 +1,16 @@
-import styles from './App.module.css';
+import styles from './styles.module.css';
 
 function App() {
   return (
     <div className={styles.App}>
+      <div className={styles.Intro}>
+        <h1>StarWars films</h1>
+        <p>A long time ago, in a galaxy far, far away...</p>
+      </div>
+
+      <div className={styles.List}>
+        <h2>Movie List:</h2>
+      </div>
     </div>
   );
 }

f006ff1eafae9680a7b8c0a7c390aca52c54c3ea
Carlos Ceniceros <juank_kenny@hotmail.com>
2022-12-05 23:06:53 -0600

    Refactor code

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import { App } from './views/App';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
